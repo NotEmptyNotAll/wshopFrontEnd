2ed9ae3425b447d2c7eb503476784405a7ab17f7
NotEmptyNotAll <shvetsvlad15@gmail.com>
2020-07-22 04:27:42 +0300

    add set size input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MONTHS} from './mock-months';
+import {element} from "protractor";
 
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+    selector: 'app-root',
+    templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 
 })
 export class AppComponent implements OnInit {
     cols: any[];
-
     title = 'select months';
     search = ''
     months = []
+    oldText = ''
+    inputErr = false
+    text = ''
+    mainColumn = [
+        {field: 'id', header: '#', width: '10%'},
+        {field: 'name', header: 'month', width: '40%'},
+    ];
 
-
-    constructor() { }
+    constructor() {
+    }
 
     ngOnInit() {
-        this.cols = [
-            { field: 'id', header: '#', width: '30%' },
-            { field: 'name', header: 'month', width: '30%' },
-        ];
+        this.cols = this.mainColumn.slice()
+    }
+
+    setColumn(): void {
+        if (this.text.match(/[^0-9,]/) === null) {
+            let sizeArr = this.text.split(',');
+            if (sizeArr.length < this.cols.length - 2 || (sizeArr.length === 1 && this.cols.length > 2 && sizeArr[0] === '')) {
+                this.cols.pop()
+            } else {
+                sizeArr.forEach((item, index) => {
+                    if (item !== '') {
+                        if (index >= this.cols.length - 2) {
+                            this.cols.push(
+                                {field: 'temp'+item, header: item, width: item + '%'},
+                            )
+                        } else if (this.cols[index + 2].width !== item + '%') {
+                            this.cols[index + 2].width = item + '%'
+                            this.cols[index + 2].header = item
+                        }
+                    }
+                })
+            }
+            this.oldText = this.text
+            this.inputErr = false
+        } else {
+            this.inputErr = true
+        }
     }
 
     onSearch(search: string): void {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {RouterModule} from '@angular/router';
+import {ReactiveFormsModule} from '@angular/forms';
 
-import { AppComponent }   from './app.component';
+import {AppComponent} from './app.component';
 
 import {TableModule} from 'primeng/table';
 import {ToastModule} from 'primeng/toast';
+import {InputMaskModule} from 'primeng/inputmask';
+
 import {CalendarModule} from 'primeng/calendar';
 import {SliderModule} from 'primeng/slider';
 import {MultiSelectModule} from 'primeng/multiselect';
@@ -20,30 +23,33 @@ import {ProgressBarModule} from 'primeng/progressbar';
 import {InputTextModule} from 'primeng/inputtext';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    TableModule,
-    CalendarModule,
-		SliderModule,
-		DialogModule,
-		MultiSelectModule,
-		ContextMenuModule,
-		DropdownModule,
-		ButtonModule,
-		ToastModule,
-    InputTextModule,
-    ProgressBarModule,
-    HttpClientModule,
-    FormsModule,
-    RouterModule.forRoot([
-      {path:'',component: AppComponent}
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        TableModule,
+        CalendarModule,
+        SliderModule,
+        DialogModule,
+        InputMaskModule,
+        MultiSelectModule,
+        ReactiveFormsModule,
+        ContextMenuModule,
+        DropdownModule,
+        ButtonModule,
+        ToastModule,
+        InputTextModule,
+        ProgressBarModule,
+        HttpClientModule,
+        FormsModule,
+        RouterModule.forRoot([
+            {path: '', component: AppComponent}
 
-		])
-  ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ],
-  providers: []
+        ])
+    ],
+    declarations: [AppComponent],
+    bootstrap: [AppComponent],
+    providers: []
 })
 
-export class AppModule { }
+export class AppModule {
+}
