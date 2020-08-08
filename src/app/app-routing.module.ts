325f52e14dc6c66fd743eb0198269f8678ddc97d
NotEmptyNotAll <shvetsvlad15@gmail.com>
2020-08-08 16:34:45 +0300

    vlad

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CreateAddComponent } from "./table-page/create-add/create-add.component";
 import {MonthsPageComponent} from "./months-page/months-page.component";
 import {RegionPageComponent} from "./region-page/region-page.component";
-import {AppComponent} from "./app.component";
 
 const routes: Routes = [
-  { path: 'region', component: MonthsPageComponent },
-  { path: 'test', component: MonthsPageComponent },
-  { path: 'ssss', component: MonthsPageComponent },
-  { path: 'rrrr', component: MonthsPageComponent },
-  { path: '', component: RegionPageComponent }
+  { path: '', component: MonthsPageComponent },
+  { path: 'region', component: RegionPageComponent }
 ];
 
 @NgModule({
